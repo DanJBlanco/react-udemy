d62663e021acafd2c02f9aadeef7130c1ea7acd2
Daniel <djblancoh@gmail.com>
2020-07-15 17:51:14 -0500

    declare variable with const or let

---
+++ intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = 'Daniel';
+let apellido = 'blanco';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre = "Daniel dentro del if";
+    let valorDado = 6;
+    console.log(valorDado, nombre);
+}
+
+console.log(valorDado);
